7fb0f95da75649b8fa6ed0eb1f08feb8c7c3428b
yonchicy <1079279184@qq.com>
2022-05-20 14:31:42 +0800

    feat(utils.h): finish logger
    there are sevaral options
    - feature out log file name
    - print to screen
    - print to disk
    - print to log file
    - print caller position
    - print in different color
    
    * examples can be found in utils.h's bottom

--- src/utils.h
+++ src/utils.h
@@ -2,48 +2,6 @@
 #ifndef EASYCC_ERROR_H
 #define EASYCC_ERROR_H
 
-#include <cstdio>
-#include <cstdlib>
-#include <cstdarg>
-#include <string>
-
-#define __DEBUG 
-#define __WARN
-#define __TRACE
-#define __INFO
-
-#ifdef __DEBUG
-#define ERROR(format, ...) \
-        do{\
-                printf("\x1b[32m""%s:%d " format "\x1b[0m\n",__FILE__,__LINE__,##__VA_ARGS__);\
-                exit(1);\
-        }\
-        while(0)
-#else 
-#define ERROR(format, ...) 
-#endif
-
-#ifdef __WARN
-#define WARNNING(format, ...) \
-            printf("\x1b[93m" format "\x1b[0m",##__VA_ARGS__); 
-#else 
-#define WARNNING(format, ...) 
-#endif
-
-
-#ifdef __INFO
-#define INFO(format, ...) \
-            printf("\x1b[34m" format "\x1b[0m",##__VA_ARGS__); 
-#else 
-#define INFO(format, ...) 
-#endif
-
-#ifdef __TRACE
-#define TRACE(format, ...) \
-            printf("\x1b[90m" format "\x1b[0m",##__VA_ARGS__); 
-#else 
-#define TRACE(format, ...) 
-#endif
 #define RED "\x1b[31m"
 #define GREEN "\x1b[32m"
 #define YELLOW "\x1b[33m"
@@ -51,53 +9,186 @@
 #define MAGENTA "\x1b[35m"
 #define CYAN "\x1b[36m"
 #define RESET "\x1b[0m"
-class logger {
-private:
-  std::string log_file_name;
-  // 是否输出到文件
-  bool to_file;
-  // 是否输出到屏幕
-  bool to_screen;
-  FILE *f;
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
 
-public:
-  void set_file(bool flag) { this->to_file = flag; }
-  void set_screen(bool flag) { this->to_screen = flag; }
-  explicit logger(const std::string &file_name, bool to_screen, bool to_file)
-      : log_file_name(file_name), to_screen(to_screen), to_file(to_file) {
-    std::string name = file_name + ".log";
-    f = fopen(name.c_str(), "w+");
-  }
-  explicit logger(const std::string &file_name, bool to_screen)
-      : logger(file_name, to_screen, false){};
-  explicit logger(const std::string &file_name) : logger(file_name, true){};
+#define __ERROR
+#define __DEBUG
+#define __WARN
+#define __TRACE
+#define __INFO
 
-  void print(const char *fmt, ...) {
-    if (to_screen) {
-      va_list args;
-      va_start(args, fmt);
-      printf(RED);
-      printf("[%s:%d]", __FILE__,__LINE__);
-      printf(fmt, args);
-      printf(RESET);
-      va_end(args);
-    }
-    if (to_file) {
-      va_list args;
-      va_start(args, fmt);
-      fprintf(f, "[%s:%d]", __FILE__,__LINE__);
-      fprintf(f, fmt, args);
-      va_end(args);
-    }
-  }
-  void println(const char *fmt, ...) {
-      va_list args;
-      va_start(args, fmt);
-      print(fmt, args);
-      printf("\n");
-      va_end(args);
-  }
-  ~logger() { fclose(f); }
+#ifdef __ERROR
+#define ERROR(format, ...)                                                     \
+    do {                                                                       \
+        printf(RED "[%s:%d]" format RESET, __FILE__, __LINE__, ##__VA_ARGS__); \
+        exit(1);                                                               \
+    } while (0)
+#else
+#define ERROR(format, ...)
+#endif
+
+#ifdef __WARN
+#define WARNNING(format, ...) \
+    printf("\x1b[93m" format "\x1b[0m", ##__VA_ARGS__);
+#else
+#define WARNNING(format, ...)
+#endif
+
+
+#ifdef __INFO
+#define INFO(format, ...) \
+    printf("\x1b[34m" format "\x1b[0m", ##__VA_ARGS__);
+#else
+#define INFO(format, ...)
+#endif
+
+#ifdef __TRACE
+#define TRACE(format, ...) \
+    printf("\x1b[90m" format "\x1b[0m", ##__VA_ARGS__);
+#else
+#define TRACE(format, ...)
+#endif
+
+enum class LOG_TYPE {
+    ERROR,
+    DEBUG,
+    WARN,
+    TRACE,
+    INFO,
 };
 
-#endif // EASYCC_ERROR_H
+
+class logger {
+private:
+    LOG_TYPE log_type;
+    std::string log_file_name;
+    // 是否输出到文件
+    bool to_file;
+    // 是否输出到屏幕
+    bool to_screen;
+    // does logger print the caller position;
+    bool print_file_position;
+    FILE *f;
+
+    void open_color() {
+
+        switch (log_type) {
+            case LOG_TYPE::ERROR:
+                printf(RED);
+                printf("ERROR|");
+                break;
+            case LOG_TYPE::DEBUG:
+                printf(GREEN);
+                printf("DEBUG|");
+                break;
+            case LOG_TYPE::WARN:
+                printf(YELLOW);
+                printf("WARN|");
+                break;
+            case LOG_TYPE::TRACE:
+                printf(MAGENTA);
+                printf("TRACE|");
+                break;
+            case LOG_TYPE::INFO:
+                printf(BLUE);
+                printf("INFO|");
+                break;
+        }
+    }
+    void close_color() {
+        printf(RESET);
+    }
+
+public:
+    explicit logger(const std::string &file_name, LOG_TYPE type, bool to_screen, bool to_file)
+        : log_file_name(file_name), log_type(type), to_screen(to_screen), to_file(to_file) ,print_file_position(true){
+        std::string name = file_name + ".log";
+        f = fopen(name.c_str(), "w+");
+    }
+    explicit logger(const std::string &file_name, LOG_TYPE type, bool to_screen)
+        : logger(file_name, type, to_screen, false){};
+    explicit logger(const std::string &file_name, LOG_TYPE type) : logger(file_name, type, true){};
+    explicit logger(const std::string &file_name) : logger(file_name, LOG_TYPE::INFO){};
+    void set_file(bool flag) { this->to_file = flag; }
+    void set_screen(bool flag) { this->to_screen = flag; }
+    void set_caller_position(bool flag){
+        this->print_file_position=flag;
+    }
+    void set_level(LOG_TYPE type) {
+        this->log_type = type;
+    }
+
+    void print(const char *fmt, ...) {
+        if (to_screen) {
+            va_list args;
+            va_start(args, fmt);
+
+            open_color();
+
+            if(print_file_position)
+                printf("[%s:%d] ", __FILE__, __LINE__);
+            printf(fmt, args);
+
+            close_color();
+
+            va_end(args);
+        }
+        if (to_file) {
+            va_list args;
+            va_start(args, fmt);
+            fprintf(f, "[%s:%d]", __FILE__, __LINE__);
+            fprintf(f, fmt, args);
+            va_end(args);
+        }
+    }
+    void println(const char *fmt, ...) {
+        va_list args;
+        va_start(args, fmt);
+        print(fmt, args);
+        printf("\n");
+        va_end(args);
+    }
+    ~logger() { fclose(f); }
+};
+class error_log : public logger{
+public:
+    explicit error_log(const std::string& name): logger(name,LOG_TYPE::ERROR){};
+
+};
+class debug_log : public logger{
+public:
+    explicit debug_log(const std::string& name): logger(name,LOG_TYPE::DEBUG){};
+
+};
+class warn_log : public logger{
+public:
+    explicit warn_log(const std::string& name): logger(name,LOG_TYPE::WARN){};
+
+};
+class info_log : public logger{
+public:
+    explicit info_log(const std::string& name): logger(name,LOG_TYPE::INFO){};
+
+};
+class trace_log : public logger{
+public:
+    explicit trace_log(const std::string& name): logger(name,LOG_TYPE::TRACE){};
+
+};
+
+//error_log test("errortest");
+//test.println("hello world");
+//
+//info_log test2("errortest");
+//test2.println("hello world");
+//warn_log test3("errortest");
+//test3.println("hello world");
+//trace_log test4("errortest");
+//test4.println("hello world");
+//debug_log test5("errortest");
+//test5.set_caller_position(false);
+//test5.println("hello world");
+#endif// EASYCC_ERROR_H
