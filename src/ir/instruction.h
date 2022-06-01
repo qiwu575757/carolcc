d1db7dc94af54138170da040b6de7afb7553e111
yonchicy <1079279184@qq.com>
2022-06-01 20:41:18 +0800

    把instruction的baseblock改成basicblock

--- src/ir/instruction.h
+++ src/ir/instruction.h
@@ -1,9 +1,9 @@
 #ifndef COMPILER_INSTRUCTION_H
 #define COMPILER_INSTRUCTION_H
-#include "baseblock.h"
+#include "basicblock.h"
 #include "user.h"
 
-class BaseBlock;
+class BasicBlock;
 class User;
 class Instruction : public User {
 public:
@@ -48,40 +48,40 @@ public:
 
 
     Instruction(Type *type, OpKind op_id, unsigned op_nums);
-    Instruction(Type *type, OpKind op_id, unsigned op_nums, BaseBlock *parent);
+    Instruction(Type *type, OpKind op_id, unsigned op_nums, BasicBlock *parent);
 
 private:
-    BaseBlock *_parent;
+    BasicBlock *_parent;
     OpKind _op_id;
 };
 
 class UnaryInst : public Instruction {
 private:
     UnaryInst(Type *type, OpKind op_id, Value *v1);
-    UnaryInst(Type *type, OpKind op_id, Value *v1, BaseBlock *parent);
+    UnaryInst(Type *type, OpKind op_id, Value *v1, BasicBlock *parent);
 
 public:
-    static UnaryInst *createNeg(Value *v1, BaseBlock *parent);
-    static UnaryInst *createNot(Value *v1, BaseBlock *parent);
+    static UnaryInst *createNeg(Value *v1, BasicBlock *parent);
+    static UnaryInst *createNot(Value *v1, BasicBlock *parent);
 };
 
 class BinaryInst : public Instruction {
 private:
     BinaryInst(Type *type, OpKind op_id, Value *v1, Value *v2);
-    BinaryInst(Type *type, OpKind op_id, Value *v1, Value *v2, BaseBlock *parent);
+    BinaryInst(Type *type, OpKind op_id, Value *v1, Value *v2, BasicBlock *parent);
 
 public:
-    static BinaryInst *createAdd(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createSub(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createMul(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createDiv(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createRem(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createShl(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createLshr(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createAshr(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createAnd(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createOr(Value *v1, Value *v2, BaseBlock *parent);
-    static BinaryInst *createXor(Value *v1, Value *v2, BaseBlock *parent);
+    static BinaryInst *createAdd(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createSub(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createMul(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createDiv(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createRem(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createShl(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createLshr(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createAshr(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createAnd(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createOr(Value *v1, Value *v2, BasicBlock *parent);
+    static BinaryInst *createXor(Value *v1, Value *v2, BasicBlock *parent);
 };
 
 class CmpInst : public Instruction {
@@ -95,27 +95,27 @@ public:
         LE
     };
     //    static CmpInst *createCmp(Type *type,CmpOp op_id,Value *v1,Value *v2);
-    static CmpInst *createEQ(Type *type, Value *v1, Value *v2, BaseBlock *parent);
-    static CmpInst *createNEQ(Type *type, Value *v1, Value *v2, BaseBlock *parent);
-    static CmpInst *createGT(Type *type, Value *v1, Value *v2, BaseBlock *parent);
-    static CmpInst *createGE(Type *type, Value *v1, Value *v2, BaseBlock *parent);
-    static CmpInst *createLT(Type *type, Value *v1, Value *v2, BaseBlock *parent);
-    static CmpInst *createLE(Type *type, Value *v1, Value *v2, BaseBlock *parent);
+    static CmpInst *createEQ(Type *type, Value *v1, Value *v2, BasicBlock *parent);
+    static CmpInst *createNEQ(Type *type, Value *v1, Value *v2, BasicBlock *parent);
+    static CmpInst *createGT(Type *type, Value *v1, Value *v2, BasicBlock *parent);
+    static CmpInst *createGE(Type *type, Value *v1, Value *v2, BasicBlock *parent);
+    static CmpInst *createLT(Type *type, Value *v1, Value *v2, BasicBlock *parent);
+    static CmpInst *createLE(Type *type, Value *v1, Value *v2, BasicBlock *parent);
 
 private:
     CmpInst(Type *type, CmpOp op_id, Value *v1, Value *v2);
-    CmpInst(Type *type, CmpOp op_id, Value *v1, Value *v2, BaseBlock *parent);
+    CmpInst(Type *type, CmpOp op_id, Value *v1, Value *v2, BasicBlock *parent);
 };
 class ReturnInst : public Instruction {
 private:
-    ReturnInst(Type *type ,BaseBlock*parent);
+    ReturnInst(Type *type ,BasicBlock*parent);
     ReturnInst(Type *type, Value *v);
-    ReturnInst(Type *type, Value *v, BaseBlock *parent);
+    ReturnInst(Type *type, Value *v, BasicBlock *parent);
     ReturnInst(Type *type, OpKind op_id, Value *v);
 
 public:
-    static ReturnInst *createRet(Value *v,BaseBlock *parent);
-    static ReturnInst *createVoidRet(BaseBlock *parent);
+    static ReturnInst *createRet(Value *v,BasicBlock *parent);
+    static ReturnInst *createVoidRet(BasicBlock *parent);
 };
 
 class BranchInst : public Instruction {
@@ -128,27 +128,27 @@ public:
 
 private:
     BrOp _br_kind;
-    BranchInst(BrOp br_op, Value *cond, Value *true_block, Value *false_block, BaseBlock *parent);
-    BranchInst(BrOp br_op, Value *cond, Value *block, BaseBlock *parent);
-    BranchInst(BrOp br_op, Value *block, BaseBlock *parent);
+    BranchInst(BrOp br_op, Value *cond, Value *true_block, Value *false_block, BasicBlock *parent);
+    BranchInst(BrOp br_op, Value *cond, Value *block, BasicBlock *parent);
+    BranchInst(BrOp br_op, Value *block, BasicBlock *parent);
 
 public:
-    static BranchInst *createIf(Value *cond, BaseBlock *true_block, BaseBlock *false_block, BaseBlock *parent);
-    static BranchInst *createWhile(Value *cond, BaseBlock *block, BaseBlock *parent);
-    static BranchInst *createBranch(BaseBlock *block, BaseBlock *parent);
+    static BranchInst *createIf(Value *cond, BasicBlock *true_block, BasicBlock *false_block, BasicBlock *parent);
+    static BranchInst *createWhile(Value *cond, BasicBlock *block, BasicBlock *parent);
+    static BranchInst *createBranch(BasicBlock *block, BasicBlock *parent);
 };
 
 class StoreInst : public Instruction{
 private:
-    StoreInst(Value *value,Value* ptr,BaseBlock *parent);
+    StoreInst(Value *value,Value* ptr,BasicBlock *parent);
 public:
-    static StoreInst *createStore(Value *value,Value* ptr,BaseBlock *parent);
+    static StoreInst *createStore(Value *value,Value* ptr,BasicBlock *parent);
 };
 class LoadInst:public Instruction{
 private:
-    LoadInst(Value* ptr,BaseBlock *parent);
+    LoadInst(Value* ptr,BasicBlock *parent);
 public:
-    static LoadInst *createLoad(Value* ptr,BaseBlock *parent);
+    static LoadInst *createLoad(Value* ptr,BasicBlock *parent);
 };
 //class GetElementPtrInst:public Instruction{
 //private:
