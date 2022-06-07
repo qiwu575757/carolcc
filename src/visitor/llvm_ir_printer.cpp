9a6af1061bc25efee6371ea86374ecbd08e95402
jiaoyuanxiang <jiaoyuanxiang@126.com>
2022-06-07 22:17:14 +0800

    UnaryInst 框架

--- src/visitor/llvm_ir_printer.cpp
+++ src/visitor/llvm_ir_printer.cpp
@@ -6,18 +6,23 @@ const std::string head="%";
 std::string getname(){
     return "%" + std::to_string(var_no++);
 }
-void SYSYBuilder::visit(tree_comp_unit &node) {
-    INFO("line:%d",node._line_no);
-    MyAssert("no function defined", node.functions.size() != 0);
-    INFO(" comp_unit functions %d definitions %d 42", node.functions.size(), node.definitions.size());
-    // 这种写法会允许 后置全局变量定义出现，这是因为语法设计的问题
-    for (auto defs: node.definitions) {
-        G_in_global_init = true;
-        defs->accept(*this);
-        G_in_global_init = false;
+void LLVMIrPrinter::visit(UnaryInst*node) {
+    
+    if(node->isNeg()){//将目的操作数的所以数据位取反加
+
+    }else if(node->isNot()){//直接加负号
+
+    }else{
+        WARNNING("null UnaryInst");
     }
-    for (auto func: node.functions) {
-        INFO("visiting %s", func->id.c_str());
-        func->accept(*this);
-    }
+
+}
+void LLVMIrPrinter::visit(BinaryInst*node) {
+    
+}
+void LLVMIrPrinter::visit(AllocaInst*node) {
+    
+}
+void LLVMIrPrinter::visit(StoreInst*node) {
+    
 }
